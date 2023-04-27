4d368f973bc4b5f9f31c9b72b029ecdc069a669f
DumbSheep123 <90939783+DumbSheep123@users.noreply.github.com>
2023-04-27 17:03:10 +0000

    Fixed Opt7

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,37 @@ function Opt(props) {
   );
 }
 
+function Opt8(props) {
+  return (
+    <>
+      <option value={props.value1}>{props.station1}</option>
+      <option value={props.value2}>{props.station2}</option>
+      <option value={props.value3}>{props.station3}</option>
+      <option value={props.value4}>{props.station4}</option>
+      <option value={props.value5}>{props.station5}</option>
+      <option value={props.value6}>{props.station6}</option>
+      <option value={props.value7}>{props.station7}</option>
+      <option value={props.value8}>{props.station8}</option>
+    </>
+  );
+}
+
+function Opt7(props) {
+  return (
+    <>
+      <option value={props.value1}>{props.station1}</option>
+      <option value={props.value2}>{props.station2}</option>
+      <option value={props.value3}>{props.station3}</option>
+      <option value={props.value4}>{props.station4}</option>
+      <option value={props.value5}>{props.station5}</option>
+      <option value={props.value6}>{props.station6}</option>
+      <option value={props.value7}>{props.station7}</option>
+      <option value={props.value8}>{props.station8}</option>
+      <option value={props.value9}>{props.station9}</option>
+    </>
+  );
+}
+
 export default function Menu() {
   return (
     <form>
@@ -35,9 +66,9 @@ export default function Menu() {
           <Opt value1="41" station1="Greenbelt" value2="42" station2="Greensboro" value3="43" station3="Grosvenor-Strathmore" value4="44" station4="Herndon" value5="45" station5="Huntington" value6="46" station6="Hyattsville Crossing" value7="47" station7="Innovation Center" value8="48" station8="Judiciary Square" value9="49" station9="King St-Old Town" value10="50" station10="L'Enfant Plaza" />
           <Opt value1="51" station1="Landover" value2="52" station2="Loudoun Gateway" value3="53" station3="McLean" value4="54" station4="McPherson Square" value5="55" station5="Medical Center" value6="56" station6="Metro Center" value7="57" station7="Minnesota Ave" value8="58" station8="Morgan Boulevard" value9="59" station9="Mt Vernon Sq 7th St-Convention Center" value10="60" station10="Navy Yard-Ballpark" />
           <Opt value1="61" station1="Naylor Road" value2="62" station2="New Corrollton" value3="63" station3="NoMa-Gallaudet U" value4="64" station4="North Bethesda" value5="65" station5="Pentagon" value6="66" station6="Pentagon City" value7="67" station7="Potomac Ave" value8="68" station8="Reston Town Center" value9="69" station9="Rhode Island Ave-Brentwood" value10="70" station10="Rockville" />
-          <Opt value1="71" station1="Ronald Reagan Washington National Airport" value2="72" station2="Rosslyn" value3="73" station3="Shady Grove" value4="74" station4="Shaw-Howard U" value5="75" station5="Silver Spring" value6="77" station6="Smithsonian" value7="78" station7="Southern Avenue" value8="79" station8="Spring Hill" value9="80" station9="Stadium-Armory" />
+          <Opt7 value1="71" station1="Ronald Reagan Washington National Airport" value2="72" station2="Rosslyn" value3="73" station3="Shady Grove" value4="74" station4="Shaw-Howard U" value5="75" station5="Silver Spring" value6="77" station6="Smithsonian" value7="78" station7="Southern Avenue" value8="79" station8="Spring Hill" value9="80" station9="Stadium-Armory" />
           <Opt value1="81" station1="Suitland" value2="82" station2="Takoma" value3="83" station3="Tenleytown-AU" value4="84" station4="Twinbrook" value5="85" station5="Tysons" value6="86" station6="U Street/African-Amer Civil War Memorial/Cardozo" value7="87" station7="Union Station" value8="88" station8="Van Dorn Street" value9="89" station9="Van Ness-UDC" value10="90" station10="Vienna/Fairfax-GMU" />
-          <Opt value1="91" station1="Virginia Square-GMU" value2="92" station2="Washington Dulles International Airport" value3="93" station3="Waterfront" value4="94" station4="West Falls Church-VT/UVA" value5="95" station5="West Hyattsville" value6="96" station6="Wheaton" value7="97" station7="Wiehle-Reston East" value8="98" station8="Woodley Park-Zoo/Adams Morgan" />
+          <Opt8 value1="91" station1="Virginia Square-GMU" value2="92" station2="Washington Dulles International Airport" value3="93" station3="Waterfront" value4="94" station4="West Falls Church-VT/UVA" value5="95" station5="West Hyattsville" value6="96" station6="Wheaton" value7="97" station7="Wiehle-Reston East" value8="98" station8="Woodley Park-Zoo/Adams Morgan" />
         </optgroup>
       </select>
       <input type="submit" value="Go >" />
